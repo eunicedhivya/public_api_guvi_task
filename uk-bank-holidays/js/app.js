a56b9af4a4e50348ec7604fa2e8b7c95065b7c82
Eunice Dhivya <divi.seed@gmail.com>
2022-07-02 18:22:15 +0530

    on selection of drop down display holidays in table format

--- uk-bank-holidays/js/app.js
+++ uk-bank-holidays/js/app.js
@@ -23,22 +23,30 @@ function showHolidays(areaname) {
 
   // const tr = document.createElement("tr")
   storeData[areaname]["events"].forEach((element) => {
+    // console.log(element);
     createTable(element);
   });
 }
 
 function createTable(rowdata) {
   const tr = document.createElement("tr");
+  console.log("rowdata", rowdata);
   // rowdata.forEach(function (itm) {
   //   const td = document.createElement("td");
   //   td.innerHTML = "text";
   //   tr.appendChild(td);
   // });
-  for (let i = 0; i < rowdata.length; i++) {
-    console.log(rowdata[i]);
+  for (const key in rowdata) {
+    console.log(`${key}: ${rowdata[key]}`);
     const td = document.createElement("td");
-    td.innerHTML = "text";
+    td.innerHTML = rowdata[key];
     tr.appendChild(td);
   }
+  // for (let i = 0; i < rowdata.length; i++) {
+  //   console.log(rowdata[i]);
+  //   // const td = document.createElement("td");
+  //   // td.innerHTML = "text";
+  //   // tr.appendChild(td);
+  // }
   tbody.appendChild(tr);
 }
